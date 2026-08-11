b6d77c29512159a5f833dfaa334af6a223cfd60c
andoricano <andoricano@gmail.com>
2026-08-11 16:14:00 +0900

    [migration] pwa-express turborepo 환경으로 migration 완료.

--- apps/client-pwa/next.config.ts
+++ apps/client-pwa/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['192.168.219.102',"192.168.219.103"],
 };
 
 export default nextConfig;
